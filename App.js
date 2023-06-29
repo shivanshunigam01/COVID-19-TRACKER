9ba2de2b0a7625fa42c882c1c6f021a1d2310ccd
SHIVANSHU NIGAM <102843159+shivanshunigam01@users.noreply.github.com>
2023-06-29 09:01:42 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Covid from './components/covid';
+
+function App() {
+  return (
+  <>
+    <Covid/>
+
+  </>
+  );
+}
+
+export default App;
